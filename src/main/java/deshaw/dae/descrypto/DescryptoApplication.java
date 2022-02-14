dd7052c7a0d538a22330b0bfe8d5f42f131cac61
Paras <parasjain99310@gmail.com>
2022-02-14 17:45:34 +0530

    init spring project

---
+++ src/main/java/deshaw/dae/descrypto/DescryptoApplication.java
@@ -0,0 +1,13 @@
+package deshaw.dae.descrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescryptoApplication.class, args);
+	}
+
+}
