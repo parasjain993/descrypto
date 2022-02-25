85094ba06657c8610621684f6b472055d51861e0
sm-code12 <malhotra.sachita3@gmail.com>
2022-02-25 14:42:11 +0530

    added login and register api

--- src/main/java/deshaw/dae/descrypto/domain/User.java
+++ src/main/java/deshaw/dae/descrypto/domain/User.java
@@ -1,31 +1,25 @@
 package deshaw.dae.descrypto.domain;
 
 public class User {
-    private Integer id;
-    private String name;
-    private String email;
+    private String username;
+    private String password;
 
-    public Integer getId() {
-        return id;
+
+    public String getUsername() {
+        return username;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getName() {
-        return name;
+    public String getPassword() {
+        return password;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
