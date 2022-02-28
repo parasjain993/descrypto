4decf48f7fbdad6cf16ea0248fe7a368492e4f6e
sonaliverma82276 <60004256+sonaliverma82276@users.noreply.github.com>
2022-02-28 18:22:02 +0530

    Update Order.java

--- src/main/java/deshaw/dae/descrypto/domain/Order.java
+++ src/main/java/deshaw/dae/descrypto/domain/Order.java
@@ -5,44 +5,15 @@ public class Order {
     private int  orderId;//primary key
     private int userId;//which user has placed
     private String orderPair;//btcusdt
-    private double amount;
+    private double amount;//what amount of your usdt wallet eg:10% 25%...
     private double limitPrice;//trade will start at this price for limit and current for market
-    private String orderType;//buy or sell
+    private double average;// average price at which trade was executed till now
+    private String orderType;//limit or market
+    private String side;// buy or sell
     private String orderStatus;//open, partially-filled, filled, cancelled
     private double filled;//how much of the order is completed
     private double total;//total spent upon buying/ total gained upon sell
 
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    public double getFilled() {
-        return filled;
-    }
-
-    public void setFilled(double filled) {
-        this.filled = filled;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
 
     public double getLimitPrice() {
         return limitPrice;
@@ -68,19 +39,50 @@ public class Order {
         this.amount = amount;
     }
 
-    public String getOrderPair() {
-        return orderPair;
+    public int getOrderId() {
+        return orderId;
     }
 
-    public void setOrderPair(String orderPair) {
-        this.orderPair = orderPair;
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
     }
 
     public String getOrderType() {
         return orderType;
     }
 
-    public void setOrderType(String orderType) {
-        this.orderType = orderType;
+    public void setOrderType(String orderType) { this.orderType = orderType; }
+
+    public String getOrderStatus() { return orderStatus; }
+
+    public void setOrderStatus(String OrderStatus) { this.orderStatus = OrderStatus; }
+
+    public String getOrderPair() { return orderPair; }
+
+    public void setOrderPair(String OrderPair) { this.orderPair = OrderPair; }
+
+    public String getSide() {
+        return side;
     }
+
+    public void setSide(String Side) { this.side = Side; }
+
+    public Double getFilled() {
+        return filled;
+    }
+
+    public void setFilled(double Filled) { this.filled = Filled; }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double Total) { this.total = Total; }
+
+    public Double getAverage() {
+        return average;
+    }
+
+    public void setAverage(Double Average) { this.average = Average; }
+
 }
