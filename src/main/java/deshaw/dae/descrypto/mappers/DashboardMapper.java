de77864fc18280dcb0b220cb271fbdb78913d88d
nehal6657 <nehalreshuaaryan@gmail.com>
2022-03-02 21:29:36 +0530

    added api to get all trading pairs

--- src/main/java/deshaw/dae/descrypto/mappers/DashboardMapper.java
+++ src/main/java/deshaw/dae/descrypto/mappers/DashboardMapper.java
@@ -1,6 +1,7 @@
 package deshaw.dae.descrypto.mappers;
 
 import deshaw.dae.descrypto.domain.AssetsAvail;
+import deshaw.dae.descrypto.domain.TradingPairs;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.List;
 @Mapper
 public interface DashboardMapper {
     List<AssetsAvail> getAllAssetsAvail();
+    List<TradingPairs> getAllTradingPairsAvail();
 
 }
