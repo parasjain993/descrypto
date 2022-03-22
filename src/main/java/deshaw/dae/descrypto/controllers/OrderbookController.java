3cb8289f7c113509bbb22f4b60e04e9cbf87968d
nehal6657 <nehalreshuaaryan@gmail.com>
2022-03-23 02:14:27 +0530

    added HATEOAS for orderbook api

--- src/main/java/deshaw/dae/descrypto/controllers/OrderbookController.java
+++ src/main/java/deshaw/dae/descrypto/controllers/OrderbookController.java
@@ -2,12 +2,17 @@ package deshaw.dae.descrypto.controllers;
 import deshaw.dae.descrypto.services.OrderbookService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 @RequestMapping("/market")
@@ -17,8 +22,9 @@ public class OrderbookController {
 
     @ApiOperation(value = "get orderbook for the given pair symbol at given instant", tags = {"Orderbook"})
     @GetMapping("/{pairSym}/orderbook")
-    public ResponseEntity<?> GetOrderBook(@PathVariable("pairSym") String pairSym){
-        return new ResponseEntity<>(orderbookService.allOpenOrdersbyPair(pairSym), HttpStatus.OK);
+    public EntityModel<?> GetOrderBook(@PathVariable("pairSym") String pairSym){
+        return  EntityModel.of(orderbookService.allOpenOrdersbyPair(pairSym),
+                linkTo(methodOn(DashboardController.class).getCoinDetails()).withRel("get Market Summary"));
     }
 
 
