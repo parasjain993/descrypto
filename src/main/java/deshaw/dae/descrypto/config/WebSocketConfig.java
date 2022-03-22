023a57aae77915a8662195882cf9100e3f05ee26
nehal6657 <nehalreshuaaryan@gmail.com>
2022-03-23 02:15:52 +0530

    websocket implementation of orderbook api

---
+++ src/main/java/deshaw/dae/descrypto/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package deshaw.dae.descrypto.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry){
+       stompEndpointRegistry.addEndpoint("/market")
+               .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry){
+        messageBrokerRegistry.enableSimpleBroker("/topic");
+        messageBrokerRegistry.setApplicationDestinationPrefixes("/app");
+    }
+}
